693eccb2de078448e97b6a0f3787e8ea2b3f5104
Eseola Aaron <aaroneseola1@gmail.com>
2025-05-12 22:03:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-/*const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
-
-menu.addEventListener('click', function () {
-        menu.classList.toggle('is-active');
-        menuLinks.classList.toggle('active');
-    });
-
-
-    document.getElementById("btnTrainees").addEventListener("click",function() {
-        window.location.href = "trainees.html";
-    });
-
-     document.getElementById("btnCareers").addEventListener("click",function() {
-        window.location.href = "/";
-    });
-
-
- document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('applicationForm');
-    const successMessage = document.getElementById('successMessage');
-
-    form.addEventListener('submit', function (e) {
-      e.preventDefault(); // Prevent actual form submission
 
       // Show the success message
       successMessage.textContent = "You’ve successfully applied!";
@@ -44,8 +20,14 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Button redirects
-    const btnTrainees = document.getElementById("btnTrainees");
-    const btnCareers = document.getElementById("btnCareers");
+     const btnTrainees = document.getElementById("btnTrainees");
+     const btnCareers = document.getElementById("btnCareers");
+     const btnHt = document.getElementById("btnHt");
+     const btnJs = document.getElementById("btnJs");
+     const btnCp = document.getElementById("btnCp");
+     const btnPyt = document.getElementById("btnPyt");
+
+
 
     if (btnTrainees) {
         btnTrainees.addEventListener("click", function () {
@@ -55,7 +37,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (btnCareers) {
         btnCareers.addEventListener("click", function () {
-            window.location.href = "/";
+            window.location.href = "webdev.html";
+        });
+    }
+
+    if (btnHt) {
+        btnHt.addEventListener("click", function () {
+            window.location.href = "https://www.udemy.com";
+        });
+    }
+
+    if (btnJs) {
+        btnJs.addEventListener("click", function () {
+            window.location.href = "https://www.coursera.org/specializations/html-css-javascript-for-web-developers";
+        });
+    }
+
+    if (btnCp) {
+        btnCp.addEventListener("click", function () {
+            window.location.href = "https://www.theknowledgeacademy.com/offers/web-development-and-web-design-certification-training-courses/?hsa_acc=9865744776&hsa_cam=16593625843&hsa_grp=1306220184589962&hsa_ad=&hsa_src=o&hsa_tgt=kwd-81638954856917:loc-188&hsa_kw=Web%20Development%20Courses&hsa_mt=e&hsa_net=adwords&hsa_ver=3&msclkid=25b838f377c11d09bffc50d128fe8909&utm_source=bing&utm_medium=cpc&utm_campaign=%5BWeb%20Development%5D%5BUK%5D&utm_term=Web%20Development%20Courses&utm_content=%5BWeb%20Development%5D%5BCourse%5D";
+        });
+    }
+
+    if (btnPyt) {
+        btnPyt.addEventListener("click", function () {
+            window.location.href = "https://www.udemy.com";
         });
     }
 
